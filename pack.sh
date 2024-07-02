665ed6c45927369d71eea0b6f29aada6a6338fad
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-07-02 22:14:16 +0200

    omptim action

--- pack.sh
+++ pack.sh
@@ -1,3 +1,6 @@
+echo "Add wasm32-unknown-unknown target..."
 rustup target add wasm32-unknown-unknown
+echo "Building wasm..."
 cargo build --release --target wasm32-unknown-unknown
+echo "Packing wasm..."
 wasm-bindgen --out-dir ./dist/ --target web ./target/wasm32-unknown-unknown/release/game.wasm
