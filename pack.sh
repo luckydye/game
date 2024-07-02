d982d09119fc5db10e66cd3be2e7785c2d6fa3e6
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-07-02 22:05:59 +0200

    update pack sctipt

--- pack.sh
+++ pack.sh
@@ -1,2 +1,3 @@
+rustup target add wasm32-unknown-unknown
 cargo build --release --target wasm32-unknown-unknown
 wasm-bindgen --out-dir ./dist/ --target web ./target/wasm32-unknown-unknown/release/game.wasm
